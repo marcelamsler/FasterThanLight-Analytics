eac804a0a5a1d77358d50a11f6f1878c4a628728
marcelamsler <marcel.amsler@gmail.com>
2015-09-23 16:31:19 +0200

    worked on node-webkit implementation

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,16 +2,24 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'MainCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'MainCtrl'
+        });
+    }])
 
-.controller('MainCtrl', [function() {
+    .controller('MainCtrl', ['$scope', function ($scope) {
 
+        var message_handler = function (message) {
+            return $scope.$apply(function () {
+                return $scope.onReceiveMessage(message);
+            });
+        };
+        new AmqpConnection(message_handler, 'confirm-test');
 
+        $scope.onReceiveMessage = function (message) {
+            console.log(message);
+        }
 
-
-}]);
+    }]);
