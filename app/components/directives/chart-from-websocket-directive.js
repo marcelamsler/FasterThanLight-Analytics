95b6daeb16b697846c3122ac5d13c68264b5ddeb
Marcel Amsler <marcelamsler@Marcels-MacBook-Pro.local>
2015-10-12 21:53:19 +0200

    added gauge and smoothed chart data

--- app/components/directives/chart-from-websocket-directive.js
+++ app/components/directives/chart-from-websocket-directive.js
@@ -5,7 +5,8 @@ angular.module('fasterThanLight').directive('chartFromWebsocket', function () {
         scope: {
             wsUrl: "@",
             minValue: "@",
-            maxValue: "@"
+            maxValue: "@",
+            messageType: "@"
         },
         link: function (scope, element, attrs) {
             var smoothie = new SmoothieChart({maxValue: scope.maxValue, minValue: scope.minValue});
@@ -13,17 +14,23 @@ angular.module('fasterThanLight').directive('chartFromWebsocket', function () {
             smoothie.streamTo(element[0], 0);
 
             var line1 = new TimeSeries();
+            var line2 = new TimeSeries();
 
             var ws = new WebSocket(scope.wsUrl);
 
             ws.onmessage = function(e){
-                var message = JSON.parse(e.data);
-                if (message.g != null) {
-                    console.log(message.timeStamp, message.g[2]);
+                var parsedMessage = JSON.parse(e.data);
+                var message = parsedMessage.genericMessage;
+                var messageType = parsedMessage.eventMessageType;
+
+                if (messageType === scope.messageType && message.g != null) {
                     line1.append(message.timeStamp, message.g[2]);
+                } else {
+                    line2.append(e.timeStamp, message.smoothValue);
                 }
             };
             smoothie.addTimeSeries(line1, {lineWidth:2,strokeStyle:'#00ff00'});
+            smoothie.addTimeSeries(line2, {lineWidth:2,strokeStyle:'#ffffff'});
         }
     };
 });
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,5 +11,31 @@ angular.module('fasterThanLight.view1', ['ngRoute'])
 
     .controller('MainCtrl', ['$scope', function ($scope) {
 
+        var lineChartData = [
 
-    }]);
+            {
+                label: "Series 1",
+                values: [{time: Date.now(), y: 100}, {time: Date.now(), y: 150}]
+            }
+        ];
+
+        var myChart = $('#gaugeChart').epoch({
+            type: 'time.gauge',
+            value: 0.5
+        });
+
+        var ws = new WebSocket("ws://localhost:8089/pilotData");
+
+        ws.onmessage = function (e) {
+            var parsedMessage = JSON.parse(e.data);
+            var message = parsedMessage.genericMessage;
+            var messageType = parsedMessage.eventMessageType;
+
+            if (messageType === "SmoothedSensorData") {
+                myChart.push(100/256*message.currentPower/100);
+            }
+        };
+
+
+    }])
+;
