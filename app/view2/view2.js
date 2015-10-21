bcffbd89c1212a122575a0b2abb26a50936c245e
Marcel Amsler <marcelamsler@Marcels-MacBook-Pro.local>
2015-10-21 15:00:01 +0200

    worked on trackpartdata show

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -17,21 +17,38 @@ angular.module('fasterThanLight.view2', ['ngRoute'])
         configureChart();
 
 
-        $scope.clearHistory = function() {
+        $scope.clearHistory = function () {
             localStorage.clear();
             $scope.dataSessions = [];
             $scope.sessions = [];
-
         };
 
         function configureChart() {
             $scope.options = {
                 lineMode: "linear",
+                drawDots: false,
+                tooltip: {mode: 'scrubber', formatter: function(x, y, series) {return y;}},
+                axes: {
+                    x: {key: 'x', type: 'date', ticks: 10},
+                    sensorData: {type: 'linear', ticks: 5, innerTicks: true, grid: true},
+                    smoothedData: {type: 'linear'},
+                    trackPartData: {type: 'line'}
+                },
                 series: [
                     {
                         y: "sensorData",
                         label: "Sensor",
-                        color: "#e377c2"
+                        color: "#3f5aff"
+                    },
+                    {
+                        y: "smoothedData",
+                        label: "Smoothed",
+                        color: "#000000"
+                    },
+                    {
+                        y: "trackPartData",
+                        label: "trackPartData",
+                        color: "#ff3333"
                     }
                 ]
             };
@@ -41,18 +58,73 @@ angular.module('fasterThanLight.view2', ['ngRoute'])
             var dataSessions = [];
             for (var session in sessions) {
                 if (sessions[session].sensorData != null) {
-                    var sessionData = sessions[session].sensorData.map(function (dataPoint) {
+                    var actualSession = sessions[session];
+                    var sessionData = actualSession.sensorData.map(function (dataPoint) {
+                        var smoothedData = getSmoothedDataForDataPoint(dataPoint, actualSession);
+                        var trackPartData = getTrackPartDataForDataPoint(dataPoint, actualSession);
+
                         return {
                             x: dataPoint.timeStamp,
                             sensorData: dataPoint.value,
-                            smoothedData: null,
-                            trackPartData: null
+                            smoothedData: smoothedData.value,
+                            trackPartData: trackPartData.value
                         }
                     });
+
+                    console.log("sessionData", sessionData);
                     dataSessions.push(sessionData);
                 }
             }
             return dataSessions;
+
+
+        }
+
+        function isSimilarTime(firstTimeStamp, secondTimeStamp) {
+            return Math.round(firstTimeStamp / 10) === Math.round(secondTimeStamp / 10);
+        }
+
+        function getSmoothedDataForDataPoint(dataPoint, session) {
+            var smoothedData = session.smoothedSensorData.filter(function (sDataPoint) {
+                return isSimilarTime(sDataPoint.timeStamp, dataPoint.timeStamp);
+            })[0];
+            if (smoothedData == null) {
+                smoothedData = {
+                    value: null
+                }
+            }
+            return smoothedData;
+        }
+
+        function getTrackPartDataForDataPoint(dataPoint, session) {
+            var trackPartData = session.trackPartData.filter(function (tDataPoint) {
+                return isSimilarTime(tDataPoint.timeStamp, dataPoint.timeStamp);
+            })[0];
+
+            if (trackPartData == null) {
+                trackPartData = {
+                    value: null
+                }
+            }
+
+            var value;
+            switch (trackPartData.value) {
+                case "LEFT":
+                    value = -5000;
+                    break;
+                case "RIGHT":
+                    value = 5000;
+                    break;
+                case "STRAIGHT":
+                    value = 0;
+                    break;
+                default :
+                    value = null;
+            }
+
+            return {
+                value: value
+            };
         }
 
         function findValidSessions() {
